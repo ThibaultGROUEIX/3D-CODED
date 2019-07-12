c48b7a8da21f0353345dceee77a071b420f7380a
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2019-07-12 22:02:16 +0200

    update download link

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,3 +1,6 @@
 #This script download the template file from ENPC cloud
-wget https://cloud.enpc.fr/s/Tqt2A3PYP29WbWk/download --no-check-certificate
-unzip download
+wget https://cloud.enpc.fr/s/WeR6zBa6jM8PDfg/download --no-check-certificate
+mv download datas_surreal_train.pth
+
+wget https://cloud.enpc.fr/s/Mq8DoBeWE74zpaY/download --no-check-certificate
+mv download datas_surreal_test.pth
--- data/download_template.sh
+++ data/download_template.sh
@@ -1,3 +1,4 @@
 #This script download the template file from ENPC cloud
 wget https://cloud.enpc.fr/s/JSZ9lyHbTp5MBwe/download --no-check-certificate
-unzip download
+mv download template_archiv
+unzip template_archiv
