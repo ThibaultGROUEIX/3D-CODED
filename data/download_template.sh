22988160e4713ad4998ac630ba65a7fdc433b821
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2019-07-15 14:50:40 -0700

    updated script

--- data/download_template.sh
+++ data/download_template.sh
@@ -1,5 +1,6 @@
 #This script download the template file from ENPC cloud
 echo "Downloading templates... "
 wget https://cloud.enpc.fr/s/JSZ9lyHbTp5MBwe/download --no-check-certificate
-mv download template_archiv
-unzip template_archiv
+mv download template_archiv.zip
+unzip template_archiv.zip
+rm template_archiv.zip
--- trained_models/download_models.sh
+++ trained_models/download_models.sh
@@ -1,4 +1,6 @@
 #This script download the models from ENPC cloud
 echo "downloading models...."
 wget https://cloud.enpc.fr/s/n4L7jqD486V8IJn/download --no-check-certificate
-unzip download
+mv download models.zip
+unzip models.zip
+rm models.zip
