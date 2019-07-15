07cf4affd3c2cf4236750e62dc3a4aeda42a19cd
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2019-07-15 14:57:34 -0700

    updated script

--- data/download_template.sh
+++ data/download_template.sh
@@ -2,5 +2,7 @@
 echo "Downloading templates... "
 wget https://cloud.enpc.fr/s/JSZ9lyHbTp5MBwe/download --no-check-certificate
 mv download data/template_archiv.zip
-unzip data/template_archiv.zip
-rm data/template_archiv.zip
+cd data
+unzip template_archiv.zip
+rm template_archiv.zip
+cd ..
--- trained_models/download_models.sh
+++ trained_models/download_models.sh
@@ -2,5 +2,7 @@
 echo "downloading models...."
 wget https://cloud.enpc.fr/s/n4L7jqD486V8IJn/download --no-check-certificate
 mv download trained_models/models.zip
-unzip trained_models/models.zip
-rm trained_models/models.zip
+cd trained_models
+unzip models.zip
+rm models.zip
+cd ../
