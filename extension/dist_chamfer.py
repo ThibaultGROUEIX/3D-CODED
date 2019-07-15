247278542e73670181e2e5103363f545ddeb0006
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2019-07-15 16:14:07 -0700

    test file for chamfer

--- extension/dist_chamfer.py
+++ extension/dist_chamfer.py
@@ -5,10 +5,7 @@ import torch
 import sys
 from numbers import Number
 from collections import Set, Mapping, deque
-print("zboub_from_dist_cham")
-
 import chamfer
-print("zboub_from_dist_cham")
 
 
 # Chamfer's distance module @thibaultgroueix
--- extension/test_chamfer.py
+++ extension/test_chamfer.py
@@ -43,10 +43,16 @@ def test_chamfer():
 	print(points1.grad, points2.grad)
 
 	mydist1, mydist2 = mydistChamfer(points1,points2)
+	print(torch.eq(dist1, mydist1))
+	print(torch.eq(dist2, mydist2))
 	print(dist1.type())
 	print(dist2.type())
 	print(mydist1.type())
 	print(mydist2.type())
+	print(dist1.size())
+	print(dist2.size())
+	print(mydist1.size())
+	print(mydist2.size())
 	assert torch.all(torch.eq(dist1, mydist1)) and torch.all(torch.eq(dist2, mydist2)) , "chamfer cuda and chamfer normal are not giving the same results"
 
 test_chamfer()
